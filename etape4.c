027838141c03d2d61f73379cf0c02745cf53b10d
Iblioce <114859927+Iblioce@users.noreply.github.com>
2024-01-25 18:37:18 +0100

    Update etape4.c

--- etape4.c
+++ etape4.c
@@ -11,15 +11,18 @@ int main() {
 	char *zone_char=malloc(sizeof(char));
 	lire=fopen("Etape4.bin","r");
 	ecrire=fopen("Reponse4.txt","w");
-	
+	size_t  c;
 	while(!feof(lire)){
 		fread( &zone_char ,sizeof(char),1,lire);
-		char dechiffre = *zone_char;
+		//char dechiffre = *zone_char;
+		size_t dechiffre = 1;
 		for(unsigned long int i=0;i<d;i++){
-			dechiffre = (dechiffre*zone_char)%d
+			//dechiffre = (dechiffre * zone_char)%d;
+			dechiffre = (dechiffre * c)%d;
 		}
-		printf("%d", dechiffre);
-		fwrite(&dechiffre,sizeof(char),1,ecrire);
+		fprintf(ecrire, "%lc", (char) dechiffre);
+		printf("%c", dechiffre);
+		fwrite(dechiffre,sizeof(char),1,ecrire);
 	}
 	fclose(lire);
 	fclose(ecrire);
