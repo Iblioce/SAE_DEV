0d2af6c92eaefa51c852397a469db39755ae2ea1
hdj176 <157647909+hdj176@users.noreply.github.com>
2024-01-25 15:15:25 +0100

    Update etape4.c

--- etape4.c
+++ etape4.c
@@ -4,25 +4,10 @@
 #include <unistd.h>
 #include <math.h>
 
-char cesar(int n,char lettre,char* alphabet,char* ALPHABET) {
-	int i=0;
-	while (lettre!=alphabet[i] | lettre!=ALPHABET[i]) {
-		i++;
-		if (i==26) {
-			return lettre;
-		}
-	}
-	if (n>i) {
-		i+=26;
-	}
-	i-=n;
-	return alphabet[i];
-}
-
 int main() {
-  long int n = 6554179; // clef publique
-  long int e = 689149 //clef publique
-  long int d = 55621 // clef privée
+	long int n = 6554179; // clef publique
+	long int e = 689149; //clef publique
+	long int d = 55621; // clef privée
 	char alphabet[26];
 	char ALPHABET[26];
 	char maj='A';
@@ -44,12 +29,9 @@ int main() {
 	lire=fopen("Etape4.bin","r");
 	ecrire=fopen("Reponse4.txt","w+");
 	
-	while (!feof(lire)) {
-		c=fread(zone_char,sizeof(char),1,lire);
-		c=cesar(13,*zone_char,alphabet,ALPHABET);
-		printf("%c",c);
-    c = (pow(c,d)%n);
-		fwrite(&c,sizeof(char),1,ecrire);
+	int b=pow(c,d)
+        c = (b%n);
+	fwrite(&c,sizeof(char),1,ecrire);
 	}
 	fclose(lire);
 	fclose(ecrire);
