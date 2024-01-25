e9c203c5f879a63212ff250d01e9ab3767962160
Iblioce <114859927+Iblioce@users.noreply.github.com>
2024-01-25 15:34:31 +0100

    Update etape4.c
    modification n°1

--- etape4.c
+++ etape4.c
@@ -6,13 +6,12 @@
 
 int main() {
 	long int n = 6554179; // clef publique
-	long int e = 689149; //clef publique
-	long int d = 55621; // clef privée
+	long int d = 55621; // clef privée utilisée pour déchiffré
+	int i = 0 ;
 	char alphabet[26];
 	char ALPHABET[26];
 	char maj='A';
 	char c='a';
-	int i=0;
 	FILE* lire;
 	FILE* ecrire;
 	char *zone_char=malloc(sizeof(char));
@@ -24,14 +23,15 @@ int main() {
 		maj++;
 		alphabet[i]=c;
 		ALPHABET[i]=maj;
-	}
-
+	}       	
 	lire=fopen("Etape4.bin","r");
 	ecrire=fopen("Reponse4.txt","w+");
-	
-	int b=pow(c,d)
-        c = (b%n);
-	fwrite(&c,sizeof(char),1,ecrire);
+	long int chiffrement = pow (c,d);
+	c = (chiffrement%n);
+	while(!feof(lire)){
+		fread( *zone_char ,sizeof(char),1,lire)
+		printf("%d",chiffrement);
+		fwrite(&c,sizeof(char),1,ecrire);
 	}
 	fclose(lire);
 	fclose(ecrire);
