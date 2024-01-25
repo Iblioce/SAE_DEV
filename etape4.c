7739140248df1e364d82ce500dad933f7fdbf17c
Iblioce <114859927+Iblioce@users.noreply.github.com>
2024-01-25 18:03:23 +0100

    Update etape4.c

--- etape4.c
+++ etape4.c
@@ -4,8 +4,8 @@
 #include <unistd.h>
 
 int main() {
-	int n = 6554179; // clef publique
-	int d = 55621; // clef privée utilisée pour déchiffré
+	unsigned long int n = 6554179; // clef publique
+	unsigned long int d = 55621; // clef privée utilisée pour déchiffré
 	FILE* lire;
 	FILE* ecrire;
 	char *zone_char=malloc(sizeof(char));
@@ -13,12 +13,13 @@ int main() {
 	ecrire=fopen("Reponse4.txt","w");
 	
 	while(!feof(lire)){
-		
 		fread( &zone_char ,sizeof(char),1,lire);
-		char chiffre = *zone_char;
-		char dechifre = (chiffre^d)%n;
-		printf("%c", dechifre);
-		fwrite(&chiffre,sizeof(char),1,ecrire);
+		char dechiffre = *zone_char;
+		for(unsigned long int i=0;i<d;i++){
+			dechiffre = (dechiffre*zone_char)%d
+		}
+		printf("%d", dechiffre);
+		fwrite(&dechiffre,sizeof(char),1,ecrire);
 	}
 	fclose(lire);
 	fclose(ecrire);
